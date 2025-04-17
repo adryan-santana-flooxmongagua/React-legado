1219e8fbbe81f40572541d111beebcbb664fd921
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-17 11:49:13 -0300

    feat: implement child components - part 1

--- reacex/ex/index.jsx
+++ reacex/ex/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Primeiro, { Segundo } from './component'; // se estiver em ./component.jsx
+import SilvaFamily from './famiyls';
 
 ReactDOM.render(
-  <div>
-    <Primeiro />
-    <Segundo />
-  </div>,
-  document.getElementById('app')
-);
+    <SilvaFamily />,
+    document.getElementById('app')
+  );
+  
